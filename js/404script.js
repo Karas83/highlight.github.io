3f21cdbe1bac466fdf56deab8b4de53cadcf8912
Rodion <sharoshkin1996@gmail.com>
2022-08-05 00:25:06 +0300

    main page done

--- js/404script.js
+++ js/404script.js
@@ -1,57 +1,15 @@
  
 
  
-$(document).ready(function () {
-  var preloader    = $('#preloader'), // селектор прелоадера
-      light        = $('.light'),
-      progressbar  = $('.dws-progress-bar'),
-      imagesCount  = $('img').length, // количество изображений
-      dBody        = $('body'), //обращаемся к body
-      percent      = 100 / imagesCount, // количество % на одну картинку
-      progress     = 0, // точка отсчета
-      imgSum       = 0, // количество картинок
-      loadedImg    = 0; // счетчик загрузки картинок
-
-  if (imagesCount >= imgSum && imagesCount > 0) {
-      preloader.css('background', '#000');
-      dBody.addClass('hidden');
-
-      $(".dws-progress-bar").circularProgress({
-          percent: 0,
-      }).circularProgress('animate', percent, 1000);
-
-      for (var i = 0; i < imagesCount; i++) { // создаем клоны изображений
-          var img_copy        = new Image();
-          img_copy.src        = document.images[i].src;
-          img_copy.onload     = img_load;
-          img_copy.onerror    = img_load;
-      }
-
-      function img_load () {
-          progress += percent;
-          loadedImg++;
-          if (progress >= 100 || loadedImg == imagesCount) {
-              preloader.delay(1000).fadeOut('slow');
-              progressbar.addClass('display-none')
-              light.addClass('light-active')
-              dBody.removeClass('hidden');
-          }
-          $(".dws-progress-bar").circularProgress('animate', progress, 500);
-      }
-  } else {
-      preloader.remove();
-  }
-
-
-});
  
  
-  function myFunction() {
+ 
+ 
+ /* Когда пользователь нажимает на кнопку, переключаться раскрывает содержимое */
+ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show1");
   }
-  function myFunction1() {
-    document.getElementById("myDropdown1").classList.toggle("show1");
-  }
+  // Закрыть раскрывающийся список, если пользователь щелкнет за его пределами.
   window.onclick = function (event) {
     if (!event.target.matches('.dropbtn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -64,26 +22,6 @@ $(document).ready(function () {
       }
     }
   }
-
-
-
- 
-
-
-  var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1.2,
-    spaceBetween: 0,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-
   const iconMenu = document.querySelector('.icon-menu');
   if (iconMenu) {
     const menuBody = document.querySelector('.menu__body');
